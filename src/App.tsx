63810e268c18570acc4f243b3a7b7a7b40b32ba7
Mike <michal.sikorski@pomeranianstartit.pl>
2023-11-04 16:58:21 +0100

    prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
 
 
+
   return (
     <>
       <div>
@@ -27,6 +28,8 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+    
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
